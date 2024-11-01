d4ac62c1d017266436f156399225b6be376edff0
puwaer <y_50uki@outlook.jp>
2024-11-01 20:53:32 +0900

    encoder

---
+++ esp32/micro_ros_for_esp32/encoder.ino
@@ -0,0 +1,39 @@
+void encoder_reset() {
+  count_l = 0;
+  count_r = 0;
+}
+
+static void enc_change_l() {
+  int a_curr = digitalRead(PIN_ENC_A_L);
+  int b_curr = digitalRead(PIN_ENC_B_L);
+
+  if(a_curr == b_curr){
+    count_l++;
+  }else{
+    count_l--;
+  }
+}
+
+static void enc_change_r() {
+  int a_curr = digitalRead(PIN_ENC_A_R);
+  int b_curr = digitalRead(PIN_ENC_B_R);
+
+  if(a_curr == b_curr){
+    count_r++;
+  }else{
+    count_r--;
+  }
+}
+
+void encoder_open() {
+  pinMode(PIN_ENC_A_L, INPUT);
+  pinMode(PIN_ENC_B_L, INPUT);
+  pinMode(PIN_ENC_A_R, INPUT);
+  pinMode(PIN_ENC_B_R, INPUT);
+  digitalWrite(PIN_ENC_A_L, HIGH);
+  digitalWrite(PIN_ENC_B_L, HIGH);
+  digitalWrite(PIN_ENC_A_R, HIGH);
+  digitalWrite(PIN_ENC_B_R, HIGH);
+  attachInterrupt(PIN_ENC_A_L, enc_change_l, CHANGE);
+  attachInterrupt(PIN_ENC_A_R, enc_change_r, CHANGE);
+}
